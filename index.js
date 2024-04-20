8843773ee34e47a00da77fe0bad57268e0d87ec6
Sara <saraatif135@gmail.com>
2024-04-21 01:48:33 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Question #10
+//this line for comment
+//in this ques we understand only the concept of comment
+console.log("sara atif");
+//Question #11
+var names = ["sara", "Aaliyan", "Tooba", "Areeba"];
+//console.log(name01[1]);
+//console.log(name01[0]);
+//console.log(name01[2]);
+//console.log(name01[3]);
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+//Queation #12
+var message = "how are you?";
+console.log("Hello, ".concat(names[1], " ").concat(message));
+//for (let name of names) {
+// console.log(`Hellow ${name}, ${message}`);
+//}
---
+++ index.ts
@@ -0,0 +1,21 @@
+//Question #10
+//this line for comment
+//in this ques we understand only the concept of comment
+console.log("sara atif");
+
+//Question #11
+let names = ["sara","Aaliyan","Tooba","Areeba"]
+//console.log(name01[1]);
+//console.log(name01[0]);
+//console.log(name01[2]);
+//console.log(name01[3]);
+for (let i = 0; i < names.length; i++ ) {
+    console.log(names[i]);
+}
+
+//Queation #12
+let message = "how are you?"
+//console.log(`Hello, ${names[1]} ${message}`);
+for (let name of names) {
+    console.log(`Hellow ${name}, ${message}`);
+}//This is the best way
